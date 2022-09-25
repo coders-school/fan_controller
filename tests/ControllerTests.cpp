9d561d6f5a7ecc7b42d32ed7a7577d9cbc3e3121
Maciej Białczak <m.bialczak@protonmail.com>
2022-09-26 00:56:43 +0200

    changed disabling test for parametrised

--- tests/ControllerTests.cpp
+++ tests/ControllerTests.cpp
@@ -19,31 +19,25 @@ using testing::ValuesIn;
 constexpr double TARGET_TEMPERATURE = 36.6;
 constexpr double TOLERANCE = 0.5;
 
-class ControllersUpdateRpmShould : public Test
+class ForTemperatureValuesWithinToleranceRange : public TestWithParam<double>
 {
   public:
-    ControllersUpdateRpmShould();
+    ForTemperatureValuesWithinToleranceRange()
+        : thermometer_(std::make_unique<NiceMock<ThermometerMock>>())
+        , fan_(std::make_unique<NiceMock<FanMock>>())
+        , sut_(*thermometer_, *fan_, TARGET_TEMPERATURE, TOLERANCE)
+    { }
 
   protected:
     std::unique_ptr<ThermometerMock> thermometer_;
     std::unique_ptr<FanMock> fan_;
     Controller sut_;
-    const double fanStartTemperature_;
-    const double nominalFanSpeedUpperThreshold_;
 };
 
-ControllersUpdateRpmShould::ControllersUpdateRpmShould()
-    : thermometer_(std::make_unique<NiceMock<ThermometerMock>>())
-    , fan_(std::make_unique<NiceMock<FanMock>>())
-    , sut_(*thermometer_, *fan_, TARGET_TEMPERATURE, TOLERANCE)
-    , fanStartTemperature_(TARGET_TEMPERATURE - TOLERANCE)
-    , nominalFanSpeedUpperThreshold_(TARGET_TEMPERATURE + TOLERANCE)
-{ }
-
-class ControllerShould : public TestWithParam<double>
+class ForTemperatureValuesBelowToleranceRange : public TestWithParam<double>
 {
   public:
-    ControllerShould()
+    ForTemperatureValuesBelowToleranceRange()
         : thermometer_(std::make_unique<NiceMock<ThermometerMock>>())
         , fan_(std::make_unique<NiceMock<FanMock>>())
         , sut_(*thermometer_, *fan_, TARGET_TEMPERATURE, TOLERANCE)
@@ -56,6 +50,7 @@ class ControllerShould : public TestWithParam<double>
 };
 
 double tempsWihtinThreshold[] = {
+    TARGET_TEMPERATURE - TOLERANCE,
     TARGET_TEMPERATURE - TOLERANCE + 0.001,
     TARGET_TEMPERATURE + TOLERANCE - 0.001,
     TARGET_TEMPERATURE - TOLERANCE * 0.99,
@@ -64,12 +59,36 @@ double tempsWihtinThreshold[] = {
     TARGET_TEMPERATURE + TOLERANCE * 0.3,
     TARGET_TEMPERATURE + TOLERANCE * 0.5,
     TARGET_TEMPERATURE + TOLERANCE * 0.99,
+    TARGET_TEMPERATURE + TOLERANCE
 };
 
-INSTANTIATE_TEST_SUITE_P(ForTemperaturesValuesWithinToleranceRange,
-                         ControllerShould,
+double tempsBelowTemperatureThreshold[] {
+    -40.0,
+    -30.0,
+    -20.0,
+    -10.0,
+    -0.001,
+    0.0,
+    0.001,
+    2.0,
+    4.0,
+    TARGET_TEMPERATURE - TOLERANCE - 0.5,
+    TARGET_TEMPERATURE - TOLERANCE - 0.1,
+    TARGET_TEMPERATURE - TOLERANCE - 0.001,
+    TARGET_TEMPERATURE - TOLERANCE - 0.0005,
+};
+
+INSTANTIATE_TEST_SUITE_P(ControllerShould,
+                         ForTemperatureValuesWithinToleranceRange,
                          ValuesIn(tempsWihtinThreshold));
 
+INSTANTIATE_TEST_SUITE_P(ControllerShould,
+                         ForTemperatureValuesBelowToleranceRange,
+                         ValuesIn(tempsBelowTemperatureThreshold));
+
+// INSTANTIATE_TEST_SUITE_P(dupa,
+//                          ControllerShould,
+//                          ValuesIn(tempsBelowTemperatureThreshold));
 // TODO: REMOVE
 /*  TESTS to do
 
@@ -84,51 +103,20 @@ INSTANTIATE_TEST_SUITE_P(ForTemperaturesValuesWithinToleranceRange,
 
 */
 
-TEST_F(ControllersUpdateRpmShould, disableFanForTemperatureBelowZero)
-{
-    EXPECT_CALL(*thermometer_, getTemperature()).WillOnce(Return(-2.2));
-    EXPECT_CALL(*fan_, disable());
-
-    sut_.updateRpm();
-}
-
-TEST_F(ControllersUpdateRpmShould, disableFanForTemperatureEqualToZero)
-{
-    EXPECT_CALL(*thermometer_, getTemperature()).WillOnce(Return(0));
-    EXPECT_CALL(*fan_, disable());
-
-    sut_.updateRpm();
-}
-
-TEST_F(ControllersUpdateRpmShould, disableFanForTemperatureSlightlyBellowStartThreshold)
-{
-    EXPECT_CALL(*thermometer_, getTemperature()).WillOnce(Return(fanStartTemperature_ - 0.001));
-    EXPECT_CALL(*fan_, disable());
-
-    sut_.updateRpm();
-}
-
-TEST_F(ControllersUpdateRpmShould, enableFanForTemperatureEqualToStartThreshold)
-{
-    EXPECT_CALL(*thermometer_, getTemperature()).WillOnce(Return(fanStartTemperature_));
-    EXPECT_CALL(*fan_, enable());
-
-    sut_.updateRpm();
-}
-
-TEST_F(ControllersUpdateRpmShould, enableFanForTemperatureEqualToTargetTempPlusTolerance)
-{
-    EXPECT_CALL(*thermometer_, getTemperature()).WillOnce(Return(nominalFanSpeedUpperThreshold_));
-    EXPECT_CALL(*fan_, enable());
-
-    sut_.updateRpm();
-}
-
-TEST_P(ControllerShould, enableFan)
+TEST_P(ForTemperatureValuesWithinToleranceRange, shouldEnableFan)
 {
     const double tempRead = GetParam();
     EXPECT_CALL(*thermometer_, getTemperature()).WillOnce(Return(tempRead));
     EXPECT_CALL(*fan_, enable());
+
+    sut_.updateRpm();
+}
+
+TEST_P(ForTemperatureValuesBelowToleranceRange, disableFan)
+{
+    const double tempRead = GetParam();
+    EXPECT_CALL(*thermometer_, getTemperature()).WillOnce(Return(tempRead));
+    EXPECT_CALL(*fan_, disable());
 
     sut_.updateRpm();
 }
